259455bc71b552be3c5924b6c280e6cfea35be63
j- <j@skeoh.com>
2019-11-12 16:02:28 +1100

    Add docs to constants file

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,11 @@
+/** For example shows 21:15 as "9:15". */
 export const TIME_FORMAT = 'h:mm';
 
+/** How frequently, in milliseconds, the clock is updated. */
 export const TIME_INTERVAL = 1000;
 
+/** How frequently, in milliseconds, the stopwatch is updated. */
 export const STOPWATCH_INTERVAL = 150;
 
+/** When, in seconds, to stop incrementing the stopwatch counter. */
 export const MAX_VALUE_SECONDS = 36000 - 1;
